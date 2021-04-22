b4a305d8160b794768b8fb52cc2776d487ca7547
Israel Douglas <israeldouglas25@gmail.com>
2021-04-22 19:52:16 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import { Confirmation } from './src/pages/Confirmation';
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const[fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return  <AppLoading/> 
+  }
+
+  return (
+    <Confirmation/> 
+  )
+}
